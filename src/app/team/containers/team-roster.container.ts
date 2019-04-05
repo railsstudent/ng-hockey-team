662fda9d7965902cc1c264f7bb1c3240a8635856
Connie Leung <cleung334@gmail.com>
2019-04-05 14:57:48 +0800

    fix:  show correct team in team roster page

--- src/app/team/containers/team-roster.container.ts
+++ src/app/team/containers/team-roster.container.ts
@@ -1,19 +1,45 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { HockeyState } from '../reducers';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+import { TeamActions } from '../actions';
+import { TeamWithPoints } from '../models';
+import { HockeyState, selectOneTeam } from '../reducers';
 
 @Component({
   templateUrl: './team-roster.container.html',
   styleUrls: ['./team-roster.container.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TeamRosterContainer implements OnInit {
-  teamId: string;
+export class TeamRosterContainer implements OnInit, OnDestroy {
+  team$: Observable<TeamWithPoints | undefined>;
+  unsubscribe$ = new Subject();
 
-  constructor(private store: Store<HockeyState>, private route: ActivatedRoute) {}
+  constructor(private store: Store<HockeyState>, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.teamId = this.route.snapshot.paramMap.get('teamId') || '';
+    const teamId = this.route.snapshot.params.teamId;
+    this.team$ = this.store.pipe(select(selectOneTeam));
+
+    this.team$
+      .pipe(
+        map(team => {
+          if (!team) {
+            return this.store.dispatch(new TeamActions.LoadTeamRoster({ teamId }));
+          }
+        }),
+        takeUntil(this.unsubscribe$),
+      )
+      .subscribe();
+  }
+
+  returnToMenu() {
+    this.router.navigate(['../../list'], { relativeTo: this.route });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
