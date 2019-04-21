0ab4ef2422341b4ff5813f9ac687dea7af31fb8a
Connie Leung <cleung334@gmail.com>
2019-04-21 16:47:54 +0800

    fix: cannot decrease number of games  to 0 if sum of goals for and goals against is greater than 0.

--- src/app/team/services/team.service.ts
+++ src/app/team/services/team.service.ts
@@ -88,6 +88,10 @@ export class TeamService {
       if (team) {
         switch (statType) {
           case UPDATE_STAT_TYPE.WIN:
+            if (team.numWin + team.numLoss + team.numDraw + delta <= 0 && team.goalsFor + team.goalsAgainst > 0) {
+              return throwError('At least one game is played when either goals for or goals against is non-zero.');
+            }
+
             if (team.numWin + delta < 0) {
               team.numWin = 0;
             } else {
@@ -95,6 +99,10 @@ export class TeamService {
             }
             break;
           case UPDATE_STAT_TYPE.LOSS:
+            if (team.numWin + team.numLoss + team.numDraw + delta <= 0 && team.goalsFor + team.goalsAgainst > 0) {
+              return throwError('At least one game is played when either goals for or goals against is non-zero.');
+            }
+
             if (team.numLoss + delta < 0) {
               team.numLoss = 0;
             } else {
@@ -102,6 +110,10 @@ export class TeamService {
             }
             break;
           case UPDATE_STAT_TYPE.DRAW:
+            if (team.numWin + team.numLoss + team.numDraw + delta <= 0 && team.goalsFor + team.goalsAgainst > 0) {
+              return throwError('At least one game is played when either goals for or goals against is non-zero.');
+            }
+
             if (team.numDraw + delta < 0) {
               team.numDraw = 0;
             } else if (team.numDraw + delta >= team.numOTWin + team.numOTLoss) {
