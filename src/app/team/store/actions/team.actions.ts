d3550b72f8e295cc97bea1c3ef3c5fb6f43a108f
Connie Leung <cleung334@gmail.com>
2019-05-20 00:03:29 +0800

    refactor: replace with new ngrx ActionCreator
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/team/store/actions/team.actions.ts
+++ src/app/team/store/actions/team.actions.ts
@@ -1,93 +1,36 @@
 import { Params } from '@angular/router';
-import { Action } from '@ngrx/store';
+import { createAction, props, union } from '@ngrx/store';
 import { Team } from '../../models';
 
-export enum TeamActionTypes {
-  LoadTeams = '[Team] Load Teams',
-  LoadTeamsSuccess = '[Team] Load Teams Success',
-  LoadTeamsFailure = '[Team] Load Teams Failed',
-  AddTeam = '[Team] Add Team',
-  AddTeamSuccess = '[Team] Add Team Success',
-  AddTeamFailure = '[Team] Add Team Failure',
-  UpdateCloseAlert = '[Team] Update Close Alert',
-  UpdateTeamRecord = '[Team] Update Team Record',
-  UpdateTeamRecordSuccess = '[Team] Update Team Record Success',
-  UpdateTeamRecordFailure = '[Team] Update Team Record Failure',
-  NavigateAction = '[Team] Route to next url',
-}
+export const LoadTeams = createAction('[Team] Load Teams');
+export const LoadTeamsSuccess = createAction('[Team] Load Teams Success', props<{ teams: Team[] }>());
+export const LoadTeamsFailure = createAction('[Team] Load Teams Failed', props<{ error: string }>());
+export const AddTeam = createAction('[Team] Add Team', props<{ division: string; name: string }>());
+export const AddTeamSuccess = createAction('[Team] Add Team Success', props<{ team: Team; message: string }>());
+export const AddTeamFailure = createAction('[Team] Add Team Failure', props<{ error: string }>());
+export const UpdateTeamRecord = createAction(
+  '[Team] Update Team Record',
+  props<{ teamId: string; delta: number; field: string }>(),
+);
+export const UpdateTeamRecordSuccess = createAction('[Team] Update Team Record Success', props<{ team: Team }>());
+export const UpdateTeamRecordFailure = createAction('[Team] Update Team Record Failure', props<{ error: string }>());
+export const UpdateCloseAlert = createAction('[Team] Update Close Alert', props<{ closeAlert: boolean }>());
+export const NavigateAction = createAction(
+  '[Team] Navigate to next url',
+  props<{ url: string; pathParams?: any[]; queryParams?: Params }>(),
+);
 
-export class LoadTeams implements Action {
-  readonly type = TeamActionTypes.LoadTeams;
-}
-
-export class LoadTeamsSuccess implements Action {
-  readonly type = TeamActionTypes.LoadTeamsSuccess;
-
-  constructor(public payload: { teams: Team[] }) {}
-}
-
-export class LoadTeamsFailure implements Action {
-  readonly type = TeamActionTypes.LoadTeamsFailure;
-
-  constructor(public payload: { error: string }) {}
-}
-
-export class AddTeam implements Action {
-  readonly type = TeamActionTypes.AddTeam;
-
-  constructor(public payload: { division: string; name: string }) {}
-}
-
-export class AddTeamSuccess implements Action {
-  readonly type = TeamActionTypes.AddTeamSuccess;
-
-  constructor(public payload: { team: Team; message: string }) {}
-}
-
-export class AddTeamFailure implements Action {
-  readonly type = TeamActionTypes.AddTeamFailure;
-
-  constructor(public payload: { error: string }) {}
-}
-
-export class UpdateTeamRecord implements Action {
-  readonly type = TeamActionTypes.UpdateTeamRecord;
-
-  constructor(public payload: { teamId: string; delta: number; field: string }) {}
-}
-
-export class UpdateTeamRecordSuccess implements Action {
-  readonly type = TeamActionTypes.UpdateTeamRecordSuccess;
-
-  constructor(public payload: { team: Team }) {}
-}
-
-export class UpdateTeamRecordFailure implements Action {
-  readonly type = TeamActionTypes.UpdateTeamRecordFailure;
-
-  constructor(public payload: { error: string }) {}
-}
-
-export class UpdateCloseAlert implements Action {
-  readonly type = TeamActionTypes.UpdateCloseAlert;
-
-  constructor(public payload: { closeAlert: boolean }) {}
-}
-
-export class NavigateAction implements Action {
-  readonly type = TeamActionTypes.NavigateAction;
-  constructor(public payload: { url: string; pathParams?: any[]; queryParams?: Params }) {}
-}
-
-export type TeamActionsUnion =
-  | LoadTeams
-  | LoadTeamsSuccess
-  | LoadTeamsFailure
-  | AddTeam
-  | AddTeamSuccess
-  | AddTeamFailure
-  | UpdateCloseAlert
-  | UpdateTeamRecord
-  | UpdateTeamRecordSuccess
-  | UpdateTeamRecordFailure
-  | NavigateAction;
+const all = union({
+  LoadTeams,
+  LoadTeamsSuccess,
+  LoadTeamsFailure,
+  AddTeam,
+  AddTeamSuccess,
+  AddTeamFailure,
+  UpdateTeamRecord,
+  UpdateTeamRecordSuccess,
+  UpdateTeamRecordFailure,
+  UpdateCloseAlert,
+  NavigateAction,
+});
+export type TeamActionsUnion = typeof all;
