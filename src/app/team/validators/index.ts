d9c1118a295dfdd109d0a5366f40c7664f7acee5
Connie Leung <cleung334@gmail.com>
2020-03-09 08:44:42 +0800

    feat: free agent validation
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/team/validators/index.ts
+++ src/app/team/validators/index.ts
@@ -2,3 +2,4 @@ export * from './minimum-age.validator';
 export * from './future-time.validator';
 export * from './uniform-num.validator';
 export * from './single-position.validator';
+export * from './free-agent.validator';
